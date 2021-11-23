1eebad46eeceac8abbcd98f2a1dd2aba77118663
TheerawuthPat <paTheerawuth@central.co.th>
2021-11-24 01:06:38 +0700

    Create Contact DB

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Contact(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    phone = models.IntegerField()
+    email = models.CharField(max_length=50)
+    
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,28 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy # new
+from .models import Contact
 
 class HomePageView(TemplateView):
+    model = Contact
     template_name = 'home.html'
 
 class OverViewPageView(TemplateView):
+    model = Contact
     template_name = 'blogs/overview.html'
 
 class UxDesignPageView(TemplateView):
+    model = Contact
     template_name = 'blogs/ux-design.html'
 
 class UiDesignPageView(TemplateView):
+    model = Contact
     template_name = 'blogs/ui-design.html'
 
 class ResearchPageView(TemplateView):
+    model = Contact
     template_name = 'blogs/research.html'
 
 class AboutPageView(TemplateView):
+    model = Contact
     template_name = 'blogs/about.html'
 
-class ContactPageView(TemplateView):
+class ContactPageView(CreateView):
+    model = Contact
     template_name = 'blogs/contact.html'
+    success_url = reverse_lazy('home')
+    fields = ['firstname', 'lastname', 'email', 'phone']
 
 class ContactListPageView(TemplateView):
+    model = Contact
     template_name = 'blogs/contact-list.html'
+    fields = ['firstname', 'lastname', 'email', 'phone']
     
